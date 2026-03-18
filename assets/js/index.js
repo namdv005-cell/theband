4552084bd9d97e1bd8ba59a7ba9c1c3372ad5389
doanhha <dhavan4444@gmail.com>
2026-03-19 01:19:50 +0700

    add function onclick to Switch to the new website and notify

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 const button = document.getElementById('btn');
+const noti = document.getElementById('notifycation');
 
 button.onclick = function() {
     window.location.href = "./index.html"
+}
+
+noti.onclick = function() {
+    alert("Xin chào Đinh Văn Nam C23-TT")
 }
